c95e9b4e5a8892506040ebf99cb92f87ba3c05dc
Sylvain Delarche <sylvain.delarche@gmail.com>
2025-10-26 18:18:30 +0100

    Improve todo project content with current progress
    I decided to only describe what my app is currently doing, and the stuff
    that is put in place.
    Cleaned up some stuff on this repo.

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ export default function Page() {
         {`Sylvain Delarche`}
       </h1>
       <p className="mb-4">
-        {`Je suis un développeur passionné, que ce soit dans le frontend avec React, ou dans le backend avec Java/Scala.`}<br />
-        {`Je suis experimenté dans la creation d'interfaces utilisateurs dynamiques, et de logique serveur robustes, avec des connaissances dans le CI/CD et le déploiement de services avec Docker et Kubernetes.`}<br />
-        {`Ma vie professionelle consiste dans la contribution à des plate-formes de services financiers et d'approvisionnement, où j'ai pu améliorer la qualité du code, l'optimiser mais aussi ajouter de nouvelles fonctionnalités.
+        {`Je suis un développeur passionné, avec des compétences avancées en frontend et backend en Java, Python et React.`}<br /><br />
+        
+        {`Je suis experimenté dans la creation d'interfaces utilisateurs dynamiques, et de logique serveur robustes, avec des connaissances dans le CI/CD et le déploiement de services avec Docker et Kubernetes.`}<br /><br />
+        
+        {`Au cours de ma vie professionnelle, j'ai pu améliorer la qualité du code, l'optimiser mais aussi ajouter de nouvelles fonctionnalités.
         Toujours dans un but de m'améliorer, je suis à la recherche de nouveaux challenges, et de nouvelles technologies à expérimenter.`}<br />
-        {`En dehors du pro, je suis un passioné de jeux vidéos ainsi que le jeu d'échec, auquel je fais parti d'un club depuis 2022.`}<br />
       </p>
       <div className="my-8">
         <BlogPosts />
